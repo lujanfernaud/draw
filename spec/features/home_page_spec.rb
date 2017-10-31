13fdc93473e11e084e768df406897c17e0eb532b
lujanfernaud <lujanfernaud@gmail.com>
2017-10-31 16:07:15 +0000

    Update home_page_spec.rb

---
+++ spec/features/home_page_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.feature "Home page", type: :feature do
+  scenario "User visits home page" do
+    visit root_url
+
+    expect(page).to have_text("draw")
+    expect_page_to_have_links_to_queries
+  end
+
+  private
+
+    def expect_page_to_have_links_to_queries
+      Request.allowed_queries.each do |query|
+        expect(page).to have_selector(:link_or_button, query)
+      end
+    end
+end
