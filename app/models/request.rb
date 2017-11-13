02a5c292b1490244218b22972552401b877dadc6
lujanfernaud <lujanfernaud@gmail.com>
2017-11-13 19:55:18 +0000

    Update query for 'face'

--- app/models/request.rb
+++ app/models/request.rb
@@ -11,7 +11,7 @@ class Request < ApplicationRecord
   def self.allowed_queries
     {"female"    => "woman-girl",
      "male"      => "man-boy",
-     "face"      => "facial",
+     "face"      => "facial-smile",
      "animal"    => "animal",
      "flower"    => "flower",
      "home"      => "home",
