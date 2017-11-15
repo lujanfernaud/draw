d1d9f56ff2bfaa409e9a67d1804e184012721c98
lujanfernaud <lujanfernaud@gmail.com>
2017-11-15 17:39:25 +0000

    Start vcr in 'describe' block

--- spec/controllers/photos_controller_spec.rb
+++ spec/controllers/photos_controller_spec.rb
@@ -14,14 +14,14 @@ RSpec.describe PhotosController, type: :controller do
     end
   end
 
-  describe "GET #show" do
-    it "responds successfully with an HTTP 200 status code", :vcr do
+  describe "GET #show", :vcr do
+    it "responds successfully with an HTTP 200 status code" do
       get :show, params: { id: "animal" }
       expect(response).to be_success
       expect(response).to have_http_status(200)
     end
 
-    it "renders the 'show' template", :vcr do
+    it "renders the 'show' template" do
       get :show, params: { id: "animal" }
       expect(response).to render_template("show")
     end
