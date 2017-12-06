00482867ec69ee39b0ae47be8b5185330252d154
lujanfernaud <lujanfernaud@gmail.com>
2017-12-06 22:16:23 +0000

    Add dropdown menu

--- spec/features/show_photos_spec.rb
+++ spec/features/show_photos_spec.rb
@@ -10,8 +10,11 @@ RSpec.feature "ShowPhotos", vcr: VCR_OPTIONS, type: :feature do
     request_photo_path(request_id: query, id: second_photo.id)
   end
 
+  let(:photo_path)       { /\photos\/\d+/ }
+  let(:query_index_path) { request_photos_path(request_id: query) }
+
   scenario "user visits photo" do
-    visit request_photo_path(request_id: query, id: first_photo.id)
+    visit_first_photo
 
     expect(page).to have_css("img")
     expect(page).to have_text("Photo by")
@@ -19,10 +22,42 @@ RSpec.feature "ShowPhotos", vcr: VCR_OPTIONS, type: :feature do
   end
 
   scenario "user clicks photo" do
-    visit request_photo_path(request_id: query, id: first_photo.id)
+    visit_first_photo
 
     find("a", class: "photo-link").click
 
     expect(page).to have_current_path(second_photo_path)
+  end
+
+  scenario "user selects 'list mode' from menu" do
+    visit_first_photo
+
+    find("a", class: "navbar-brand").click
+    click_on "list mode"
+
+    expect(page).to have_current_path(query_index_path)
+  end
+
+  scenario "user selects 'focus mode' from menu" do
+    visit_first_photo
+
+    find("a", class: "navbar-brand").click
+    click_on "list mode"
+    click_on "focus mode"
+
+    expect(current_path).to match(photo_path)
+  end
+
+  scenario "user goes back home" do
+    visit_first_photo
+
+    find("a", class: "navbar-brand").click
+    click_on "home"
+
+    expect(page).to have_current_path(root_path)
+  end
+
+  def visit_first_photo
+    visit request_photo_path(request_id: query, id: first_photo.id)
   end
 end
